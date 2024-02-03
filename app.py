ea6773c3a6522621eeb9ae0c644dd261f3be7d65
Tanishka <tanishkagiri58@gmail.com>
2024-02-03 17:23:53 +0530

    first space commit

--- app.py
+++ app.py
@@ -1,38 +1,41 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__)#initialize flask
 
-@app.route("/")
+@app.route("/")  #route your webpage
+
 def visitors():
 
-    # Load current count
-    counter_read_file = open("count.txt", "r")
-    visitors_count = int(counter_read_file.read())
-    counter_read_file.close()
+	# Load current count
+	counter_read_file = open("count.txt", "r")
+	visitors_count = int(counter_read_file.read())
+	counter_read_file.close()
 
-    # Increment the count
-    visitors_count = visitors_count + 1
+	# Increment the count
+	visitors_count = visitors_count + 1
 
-    # Overwrite the count
-    counter_write_file = open("count.txt", "w")
-    counter_write_file.write(str(visitors_count))
-    counter_write_file.close()
+	# Overwrite the count
+	counter_write_file = open("count.txt", "w")
+	counter_write_file.write(str(visitors_count))
+	counter_write_file.close()
 
-    # Render HTML with count variable
-    return render_template("index.html", count=visitors_count)
+	return render_template("index.html", count=visitors_count)
 
-@app.route('/', methods=['POST'])
+
+@app.route('/',methods=['POST'])
+#route your webpage
 def covid_stats():
-    # Load current count
-    counter_read_file = open("count.txt", "r")
-    visitors_count = int(counter_read_file.read())
-    counter_read_file.close()
+	# Load current count
+	counter_read_file = open("count.txt", "r")
+	visitors_count = int(counter_read_file.read())
+	counter_read_file.close()
 
-    text = request.form['text']
+	text = request.form['text']
 
-    corona_data = 'https://corona.dnsforfamily.com/graph.png?c='+text
-    print(corona_data)
-    return render_template("index.html", image=corona_data, count=visitors_count)
+	corona_data = 'https://covidstats-sdbd.onrender.com/?country='+text
+	print(corona_data)
+	return render_template("index.html", image=corona_data, count=visitors_count)
+
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
